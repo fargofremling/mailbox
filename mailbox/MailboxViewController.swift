d2b410539a9530bde54d84a74d4e18fe9e311ace
fargofremling <alicia.fremling@gmail.com>
2016-02-20 21:00:16 -0800

    Set up initial pan functions

--- mailbox/MailboxViewController.swift
+++ mailbox/MailboxViewController.swift
@@ -11,10 +11,22 @@ import UIKit
 class MailboxViewController: UIViewController {
 
     @IBOutlet weak var mailboxScrollView: UIScrollView!
+    @IBOutlet weak var messageView: UIView!
+    @IBOutlet weak var deleteView: UIImageView!
+    @IBOutlet weak var archiveView: UIImageView!
+    @IBOutlet weak var rescheduleView: UIImageView!
+    @IBOutlet weak var listView: UIImageView!
+    @IBOutlet weak var subjectView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.mailboxScrollView.contentSize = CGSize (width: 320, height: 1432)
-
+        deleteView.alpha = 0
+        archiveView.alpha = 0
+        rescheduleView.alpha = 0
+        listView.alpha = 0
+        subjectView.alpha = 1
+        
         // Do any additional setup after loading the view.
     }
 
@@ -23,6 +35,56 @@ class MailboxViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+
+    @IBAction func didPanMessage(sender: UIPanGestureRecognizer) {
+        
+        var location = sender.locationInView(view)
+        var translation = sender.translationInView(view)
+        var velocity = sender.velocityInView(view)
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            
+        }
+        
+        else if sender.state == UIGestureRecognizerState.Changed {
+            if velocity.x > 0 {
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    self.rescheduleReveal()
+                })
+            }
+            else {
+                UIView.animateWithDuration(0.3, animations:
+                    { () -> Void in
+                    self.deleteReveal()
+                })
+            }
+            
+        }
+        else if sender.state == UIGestureRecognizerState.Ended {
+        
+        }
+    }
+    
+    func rescheduleReveal () {
+        messageView.backgroundColor = UIColor.yellowColor()
+        rescheduleView.alpha = 1
+        
+    }
+    
+    func listReveal () {
+        messageView.backgroundColor = UIColor.brownColor()
+        listView.alpha = 1
+    }
+    
+    func archiveReveal () {
+        messageView.backgroundColor = UIColor.greenColor()
+        archiveView.alpha = 1
+    }
+    
+    func deleteReveal () {
+        messageView.backgroundColor = UIColor.redColor()
+        deleteView.alpha = 1
+    }
 
     /*
     // MARK: - Navigation
